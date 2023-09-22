f27d019f37d8d4ade78edfdee651e1156bab12d7
MarGama <210358@student.glu.nl>
2023-09-22 14:44:49 +0200

    c

---
+++ Godot-dungeon-crawler/TestScript.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var country_name : String = "albania"
+var population : int = 5
+var highest_altitude : float = 12.5
+var is_landlocked : bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
